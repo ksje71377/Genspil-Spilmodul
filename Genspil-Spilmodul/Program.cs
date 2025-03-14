20ce6f561f234a7a686197be3968a309449f7c81
ksje713777 <ksje71377@edu.ucl.dk>
2025-03-14 22:02:37 +0100

    Add project files.

---
+++ Genspil-Spilmodul/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Genspil_Spilmodul
+{
+    class Game
+    {
+        private string title;
+        private string genre;
+        private string variant;
+        private string developer;
+
+    }
+    class Client
+    {
+        private string name;
+        private string address;
+        private string email;
+        private string phoneNumber;
+        private string city;
+        private string postalCode;
+    }
+    public enum AgeRating
+    {
+        from0,
+        from3,
+        from7,
+        from11,
+        from15,
+        from18,
+    }
+    public enum GameState
+    {
+        inStock,
+        notInStock,
+        requested,
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
