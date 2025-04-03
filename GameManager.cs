e39b5f8e02124340eea52f271b7c19a171c53033
ksje713777 <ksje71377@edu.ucl.dk>
2025-04-03 12:01:53 +0200

    WIP - prøver at tilpasse syntaks i filhåndtering for at forhåbentlig få et fungerende resultat.

--- GameManager.cs
+++ GameManager.cs
@@ -7,7 +7,9 @@ namespace Genspil_Spilmodul
 {
     public class GameManager
     {
-        string filePath = "gameDatabase.txt";
+        //Stien til at gemme fil databasen. Bemærk const, for at den ikke kan ændres.
+        const string filePath = "gameDatabase.txt";
+
         public static void ReadFromFile(string filePath)
         {             //Metoden ReadFromFile læser fra en fil, og returnerer en string.
             StreamReader reader = new StreamReader(filePath);
@@ -19,23 +21,35 @@ namespace Genspil_Spilmodul
                 reader.Close();
         }
                 {   
+
         //Metoden Countdatabase tæller hvor mange spil der totalt er oprettet i databasen.
                 StreamReader reader = new StreamReader(filePath);
+
+                //EndOfStream betyder ved enden af listen
+
                 while (!reader.EndOfStream)
                 {
                     Console.WriteLine(reader.ReadLine());
                 count++;
                 } //Der er {0} spil i databasen, count);
 
-    reader.Close();
-        }
+               //Console.WriteLine("Der er {0} spil i databasen", count);
+
+               //reader og writer skal altid lukkes, når de ikke længere er i brug.
+                 reader.Close();
+                }
+
         public static void WriteToFile(string filePath, string content)
         {
             //Metoden WriteToFile skriver til en fil, og tager en string som parameter.
+
             StreamWriter writer = new StreamWriter(filePath, true);
+
             //(filePath, true) betyder at vi tilføjer til filen, og ikke overskriver den.
+
             writer.WriteLine(content);
-            writer.Close();
+        //Som før, husker vi at Close for at undgå problemer senere hen. 
+        writer.Close();
         }
 
         // String-interpolation med $ og {} til at indsætte variabelværdier i en streng
@@ -43,6 +57,7 @@ namespace Genspil_Spilmodul
         { 
         return $"{nTitle},{nGenre},{nPrice},{nDeveloper}, {nCondition},{nAgeRating},{nPlayerNumbers}";
         }
+
         //Method CreateGame opretter et nyt spil i lagerbeholdningen.
         public void CreateGame()
         {
@@ -54,20 +69,23 @@ namespace Genspil_Spilmodul
         {
         Id = currentId++;
         }
-}
+        }
+
         //Method EditGame styrer redigering af spil i lagerbeholdningen
         public void EditGame()
         {
         }
+
         //Method EditGame sørger for sletning af spil i lagerbeholdningen
         public void DeleteGame()
         {
         }
-// Metode til at gemme en liste af afdelinger i en fil
-public void SaveGamesToFile(List<Game> games)
-{
-    using (StreamWriter sw = new StreamWriter(filePath)) // Åbner filen til skrivning
-    {
+
+        // Metode til at gemme en liste af afdelinger i en fil
+        public void SaveGamesToFile(List<Game> games)
+        {
+        using (StreamWriter sw = new StreamWriter(filePath)) // Åbner filen til skrivning
+        {
         foreach (var game in games)
         {
             foreach (var gameList in games)
